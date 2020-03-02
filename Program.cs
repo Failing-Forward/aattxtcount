16b66234dbec275bc245015043a8e015ecd193de
Dmitry Mikhaltsov <45086516+bqio@users.noreply.github.com>
2020-03-02 06:33:47 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System;
+
+namespace aattxtcount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                return;
+            }
+
+            string text = File.ReadAllText(args[0]);
+            Regex regex = new Regex(@"[а-яА-Я][.,~`!'@#№$;%:^?&*()-+\\/]");
+            MatchCollection matches = regex.Matches(text);
+            List<string> Variants = new List<string>();
+            string dist = "";
+
+            if (matches.Count > 0)
+            {
+                foreach(Match match in matches)
+                {
+                    if (!Variants.Contains(match.Value)) {
+                        Variants.Add(match.Value);
+                        dist += match.Value + Environment.NewLine;
+                    }
+                }
+                File.WriteAllText(Path.GetFileNameWithoutExtension(args[0]) + "_list.txt", dist.Substring(0, dist.Length - 2));
+            } else {
+                Console.WriteLine("Not found.");
+            }
+        }
+    }
+}
